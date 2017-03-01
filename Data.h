6d7c3c70f19cbb8d046f550bad31222542768c94
yongrenwang <czie.alexmak@gmail.com>
2017-03-01 12:22:29 +0800

    Added Data class, defined constants for dictionary keys

---
+++ Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Plain 'Ol Notes
+//
+//  Created by Aleksander Makedonski on 3/1/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+#define kDefaultText @"New Note"
+#define kAllNotes @"notes"
+#define kDetailView @"showDetail"
+
+@interface Data : NSObject
+
+@end
+
---
+++ Data.m
@@ -0,0 +1,16 @@
+//
+//  Data.m
+//  Plain 'Ol Notes
+//
+//  Created by Aleksander Makedonski on 3/1/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Data.h"
+
+@implementation Data
+
+
+
+@end
