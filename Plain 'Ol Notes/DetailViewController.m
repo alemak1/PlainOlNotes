5fadcb1fc479bec824e910db7ccb1839d2a7d0a2
yongrenwang <czie.alexmak@gmail.com>
2017-03-01 14:18:39 +0800

    Refactored the configureView method so that the text in the textView is set equal to the currentNote if the value for the currentKey in the dictionary is not the kDefaultText value; in the setDetailItem method, the current key is set to the current date

--- Plain 'Ol Notes/DetailViewController.m
+++ Plain 'Ol Notes/DetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DetailViewController.h"
+#import "Data.h"
 
 @interface DetailViewController ()
 
@@ -16,9 +17,14 @@
 
 - (void)configureView {
     // Update the user interface for the detail item.
-    if (self.detailItem) {
-        self.tView.text = [self.detailItem description];
+    NSString* currentNote = [[Data getAllNotes] objectForKey:[Data getCurrentKey]];
+    if(![currentNote isEqualToString:kDefaultText]){
+        self.tView.text = currentNote;
+    } else {
+        self.tView.text = @"";
     }
+    
+    [self.tView becomeFirstResponder];
 }
 
 
@@ -40,6 +46,7 @@
 - (void)setDetailItem:(NSDate *)newDetailItem {
     if (_detailItem != newDetailItem) {
         _detailItem = newDetailItem;
+        [Data setCurrentKey:_detailItem];
         
         // Update the view.
         [self configureView];
